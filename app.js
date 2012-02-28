66727062edaca8d531953550f1e0f1e218b2e661
Aseem Kishore <aseem.kishore@gmail.com>
2012-02-28 04:02:17 -0800

    Herok support! Procfile + PORT env var.

--- app.js
+++ app.js
@@ -32,5 +32,5 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
