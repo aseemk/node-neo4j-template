f684777dcdeb743ce0f2454d290ef17f7ecbd1b5
Aseem Kishore <aseem.kishore@gmail.com>
2014-02-09 17:16:21 -0500

    Support Heroku GrapheneDB add-on.

--- models/user.js
+++ models/user.js
@@ -2,7 +2,11 @@
 // User model logic.
 
 var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase(process.env.NEO4J_URL || 'http://localhost:7474');
+var db = new neo4j.GraphDatabase(
+    process.env['NEO4J_URL'] ||
+    process.env['GRAPHENEDB_URL'] ||
+    'http://localhost:7474'
+);
 
 // constants:
 
