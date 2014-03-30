7bb815ffba4a26d84c6324be4309113f002aa361
Aseem Kishore <aseem.kishore@gmail.com>
2014-03-30 18:51:28 -0400

    Implement two-user following tests!

--- test/models/user.js
+++ test/models/user.js
@@ -18,13 +18,23 @@
 // - List users again; should be back to initial list.
 //
 // - Create two users in parallel, B and C.
+// - Fetch both user's "following and others"; both should show no following.
 // - Have user B follow user C.
-// - Attempt to have user B follow user C again; should be idempotent.
-// - Fetch both users' "following and others"; should be right.
+// - Have user B follow user C again; should be idempotent.
+// - Fetch user B's "following and others"; should show following user C.
+// - Fetch user C's "following and others"; should show not following user B.
 // - Have user B unfollow user C.
-// - Attempt to have user B unfollow user C again; should be idempotent.
-// - Fetch both users' "following and others" again; should be changed.
-// - Delete both users.
+// - Have user B unfollow user C again; should be idempotent.
+// - Fetch both users' "following and others" again; both should follow none.
+//
+// - Create a user D.
+// - Have user B follow user C follow user D.
+// - Fetch all users' "following and others"; should be right.
+// - Delete user B.
+// - Fetch user C's and D's "following and others"; should be right.
+// - Delete user D.
+// - Fetch user C's "following and others"; should be right.
+// - Delete user C.
 //
 // NOTE: I struggle to translate this kind of test plan into BDD style tests.
 // E.g. what am I "describing", and what should "it" do?? Help welcome! =)
@@ -38,7 +48,7 @@ var User = require('../../models/user');
 // Shared state:
 
 var INITIAL_USERS;
-var USER_A, USER_B, USER_C;
+var USER_A, USER_B, USER_C, USER_D;
 
 
 // Helpers:
@@ -57,6 +67,34 @@ function expectUser(obj, user) {
             expect(obj[prop]).to.equal(user[prop]);
         });
     }
+}
+
+/**
+ * Asserts that the given array of users contains the given user,
+ * exactly and only once.
+ */
+function expectUsersToContain(users, expUser) {
+    var found = false;
+
+    expect(users).to.be.an('array');
+    users.forEach(function (actUser) {
+        if (actUser.id === expUser.id) {
+            expect(found, 'User already found').to.equal(false);
+            expectUser(actUser, expUser);
+            found = true;
+        }
+    });
+    expect(found, 'User not found').to.equal(true);
+}
+
+/**
+ * Asserts that the given array of users does *not* contain the given user.
+ */
+function expectUsersToNotContain(users, expUser) {
+    expect(users).to.be.an('array');
+    users.forEach(function (actUser) {
+        expect(actUser.id).to.not.equal(expUser.id);
+    });
 }
 
 
@@ -110,16 +148,7 @@ describe('User models:', function () {
             // new array is one longer than the initial, and contains user A.
             expect(users).to.be.an('array');
             expect(users).to.have.length(INITIAL_USERS.length + 1);
-
-            var found = false;
-            users.forEach(function (user) {
-                if (user.id === USER_A.id) {
-                    expect(found, 'User A already found').to.equal(false);
-                    expectUser(user, USER_A);
-                    found = true;
-                }
-            });
-            expect(found, 'User A not found').to.equal(true);
+            expectUsersToContain(users, USER_A);
 
             return next();
         });
@@ -163,30 +192,184 @@ describe('User models:', function () {
             // initial length, and *doesn't* contain user A.
             expect(users).to.be.an('array');
             expect(users).to.have.length(INITIAL_USERS.length);
-            users.forEach(function (user) {
-                expect(user.id).to.not.equal(USER_A.id);
-            });
+            expectUsersToNotContain(users, USER_A);
 
             return next();
         });
     });
 
-    // Multi-user following:
+    // Two-user following:
 
-    it('Create users B and C');
+    it('Create users B and C', function (next) {
+        var nameB = 'Test User B';
+        var nameC = 'Test User C';
 
-    it('Have user B follow user C');
+        function callback(err, user) {
+            if (err) return next(err);
 
-    it('Attempt to have user B follow user C again');
+            expectUser(user);
 
-    it('Fetch both users’ “following and others”');
+            switch (user.name) {
+                case nameB:
+                    USER_B = user;
+                    break;
+                case nameC:
+                    USER_C = user;
+                    break;
+                default:
+                    // trigger an assertion error:
+                    expect(user.name).to.equal(nameB);
+            }
 
-    it('Have user B unfollow user C');
+            if (USER_B && USER_C) {
+                return next();
+            }
+        }
 
-    it('Attempt to have user B unfollow user C again');
+        User.create({name: nameB}, callback);
+        User.create({name: nameC}, callback);
+    });
 
-    it('Fetch both users’ “following and others” again');
+    it('Fetch user B’s “following and others”', function (next) {
+        USER_B.getFollowingAndOthers(function (err, following, others) {
+            if (err) return next(err);
 
-    it('Delete both users');
+            // following should be empty, and others should be all users
+            // minus user B. that's equal to initial users plus user C.
+            expect(following).to.be.an('array');
+            expect(following).to.be.empty;
+            expect(others).to.be.an('array');
+            expect(others).to.have.length(INITIAL_USERS.length + 1);
+            expectUsersToContain(others, USER_C);
+            expectUsersToNotContain(others, USER_B);
+
+            return next();
+        });
+    });
+
+    it('Fetch user C’s “following and others”', function (next) {
+        USER_C.getFollowingAndOthers(function (err, following, others) {
+            if (err) return next(err);
+
+            // similar to the previous, but switching user B and C.
+            expect(following).to.be.an('array');
+            expect(following).to.be.empty;
+            expect(others).to.be.an('array');
+            expect(others).to.have.length(INITIAL_USERS.length + 1);
+            expectUsersToContain(others, USER_B);
+            expectUsersToNotContain(others, USER_C);
+
+            return next();
+        });
+    });
+
+    it('Have user B follow user C', function (next) {
+        USER_B.follow(USER_C, function (err) {
+            return next(err);
+        });
+    });
+
+    it('Have user B follow user C again', function (next) {
+        USER_B.follow(USER_C, function (err) {
+            return next(err);
+        });
+    });
+
+    it('Fetch user B’s “following and others”', function (next) {
+        USER_B.getFollowingAndOthers(function (err, following, others) {
+            if (err) return next(err);
+
+            // following should now have (only) user C, and others should have
+            // everyone else.
+            expect(following).to.be.an('array');
+            expect(following).to.have.length(1);
+            expectUsersToContain(following, USER_C);
+            expect(others).to.be.an('array');
+            expect(others).to.have.length(INITIAL_USERS.length);
+            expectUsersToNotContain(others, USER_B);
+            expectUsersToNotContain(others, USER_C);
+
+            return next();
+        });
+    });
+
+    it('Fetch user C’s “following and others”', function (next) {
+        USER_C.getFollowingAndOthers(function (err, following, others) {
+            if (err) return next(err);
+
+            // C's results should be unchanged from before:
+            expect(following).to.be.an('array');
+            expect(following).to.be.empty;
+            expect(others).to.be.an('array');
+            expect(others).to.have.length(INITIAL_USERS.length + 1);
+            expectUsersToContain(others, USER_B);
+            expectUsersToNotContain(others, USER_C);
+
+            return next();
+        });
+    });
+
+    it('Have user B unfollow user C', function (next) {
+        USER_B.unfollow(USER_C, function (err) {
+            return next(err);
+        });
+    });
+
+    // NOTE: skipping this actually causes the next two tests to fail!
+    it('Have user B unfollow user C again', function (next) {
+        USER_B.unfollow(USER_C, function (err) {
+            return next(err);
+        });
+    });
+
+    it('Fetch user B’s “following and others”', function (next) {
+        USER_B.getFollowingAndOthers(function (err, following, others) {
+            if (err) return next(err);
+
+            // these should be back to the way they originally were:
+            expect(following).to.be.an('array');
+            expect(following).to.be.empty;
+            expect(others).to.be.an('array');
+            expect(others).to.have.length(INITIAL_USERS.length + 1);
+            expectUsersToContain(others, USER_C);
+            expectUsersToNotContain(others, USER_B);
+
+            return next();
+        });
+    });
+
+    it('Fetch user C’s “following and others”', function (next) {
+        USER_C.getFollowingAndOthers(function (err, following, others) {
+            if (err) return next(err);
+
+            // and these should still be the same:
+            expect(following).to.be.an('array');
+            expect(following).to.be.empty;
+            expect(others).to.be.an('array');
+            expect(others).to.have.length(INITIAL_USERS.length + 1);
+            expectUsersToContain(others, USER_B);
+            expectUsersToNotContain(others, USER_C);
+
+            return next();
+        });
+    });
+
+    // Multi-user-following deletions:
+
+    it('Create user D');
+
+    it('Have user B follow user C follow user D');
+
+    it('Fetch all user’s “following and others”');
+
+    it('Delete user B');
+
+    it('Fetch user C’s and D’s “following and others”');
+
+    it('Delete user D');
+
+    it('Fetch user C’s “following and others”');
+
+    it('Delete user C');
 
 });
